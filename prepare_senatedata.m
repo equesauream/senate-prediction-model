e74fdb6333c11cf8eb6f66380cf6050c30440f77
clyde <frankwang4164@gmail.com>
2025-12-26 18:41:43 -0500

    slight changes to senate project

---
+++ prepare_senatedata.m
@@ -0,0 +1,12 @@
+function A = prepare_senatedata()
+%   A0: A numeric matrix (one row per student, one column per field)
+%   fieldnames: A cell array of strings containing the extracted column headers
+
+    T = readtable('114_congress.csv', 'Delimiter', ',');
+    
+    T = T(:, 4:end);
+
+    A = table2array(T);
+    A(A == 0) = -1;
+    A(A == 0.5) = 0;
+end
